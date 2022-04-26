4878da7918e7da96d0019d5f5409965f012c4f03
psions <psionreality@gmail.com>
2022-04-26 14:32:54 -0500

    created multiple endpoints to get info via publisher or id lookup

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,39 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    // api/games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+
+            return Ok(videoGamePublishers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesByPublisher(int id)
+        {
+            int? maxYear = _context.VideoGames.Select(vg =>vg.Year).Max();
+            int? minYear = _context.VideoGames.Select(vg => vg.Year).Min();
+
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
+            return Ok(videoGames);
+        }
+    }
+
+
+}
--- ASP_NET_Video_Games_API/Data/ApplicationDbContext.cs
+++ ASP_NET_Video_Games_API/Data/ApplicationDbContext.cs
@@ -6,6 +6,10 @@ namespace ASP_NET_Video_Games_API.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<VideoGame> VideoGames { get; set; }
+
+
+
+
         public ApplicationDbContext(DbContextOptions options)
             : base(options)
         {
